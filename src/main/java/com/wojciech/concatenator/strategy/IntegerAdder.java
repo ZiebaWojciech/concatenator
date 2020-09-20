601e3a7d00b4c852bd2d4d6c6d122e5262904902
Wojciech Z <34630361+ZiebaWojciech@users.noreply.github.com>
2020-09-20 12:18:25 +0200

    Bulletproof the adding conatenator (#1)
    * adding two integers
    
    * replace null input with zero (valid assumption for integer input)
    
    Co-authored-by: Wojciech Zieba <wzieba@amelco.co.uk>

--- src/main/java/com/wojciech/concatenator/strategy/IntegerAdder.java
+++ src/main/java/com/wojciech/concatenator/strategy/IntegerAdder.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Component;
 public class IntegerAdder implements ConcatenationStrategy<Integer> {
     @Override
     public Integer concatenate(Integer first, Integer second) {
+        first = replaceNullWithZero(first);
+        second = replaceNullWithZero(second);
         return first + second;
+    }
+
+    private Integer replaceNullWithZero(Integer value) {
+        return value == null ? 0 : value;
     }
 }
