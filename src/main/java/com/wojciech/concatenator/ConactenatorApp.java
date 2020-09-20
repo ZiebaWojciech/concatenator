b9b83e9ba991e610322c61fa244317b7ed833dd8
Wojciech Zieba <wzieba@amelco.co.uk>
2020-09-20 10:09:07 +0200

    setup new springboot application

---
+++ src/main/java/com/wojciech/concatenator/ConactenatorApp.java
@@ -0,0 +1,11 @@
+package com.wojciech.concatenator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConactenatorApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConactenatorApp.class, args);
+    }
+}
